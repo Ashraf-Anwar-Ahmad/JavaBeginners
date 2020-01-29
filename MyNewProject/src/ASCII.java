a7d9b702e8dac896cf45e8cf5163d638c76fa9c1
Anwar <ashrafanwar864@gmail.com>
2020-01-29 11:13:32 +0530

    ASCII

--- MyNewProject/src/ASCII.java
+++ MyNewProject/src/ASCII.java
@@ -3,6 +3,10 @@ public class ASCII
 {
 	public static void main(String ab[])
 	{
+		//ASCII -> American Standard Code for Information Interchange
+		//7bits
+		//2^7=128
+		//0-127
 		for(int i=0;i<=127;i++)
 			System.out.printf("%d : %c\n",i,i);
 	}
