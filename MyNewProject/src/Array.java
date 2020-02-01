2927d6bb7abea8d2cde04257ccacf77b5320b5e7
Anwar <ashrafanwar864@gmail.com>
2020-02-01 19:50:10 +0530

    Array 1-D

---
+++ MyNewProject/src/Array.java
@@ -0,0 +1,14 @@
+
+public class Array {
+	public  static void main(String ab[])
+	{	//one dimensional array
+		int a[]=new int[5];
+		a[0]=5;
+		a[1]=7;
+		a[2]=9;
+		a[3]=6;
+		a[4]=8;
+		for(int i=0;i<5;i++)
+			System.out.println(i+" "+a[i]);
+	}
+}
