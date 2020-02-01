671135ab279d9ff210456445e87dc88c3afd66d6
Anwar <ashrafanwar864@gmail.com>
2020-02-01 19:50:52 +0530

    Prime

--- hello/src/Prime.java
+++ hello/src/Prime.java
@@ -8,9 +8,11 @@ public class Prime
 		Scanner ek=new Scanner(System.in);
 		n=ek.nextInt();
 		for(i=2;i<n;i++)
+		{
 			if(n%i==0)
 				break;
-			if(i==n) 
+		}
+		if(i==n) 
 			System.out.println("Prime Number");
 		else
 			System.out.print("Not a Prime Number");
