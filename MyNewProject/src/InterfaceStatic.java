7c41f61ad11df538b3c5473fbb43385aa1256f9a
Anwar <ashrafanwar864@gmail.com>
2020-02-24 23:25:45 +0530

    Static Interface

---
+++ MyNewProject/src/InterfaceStatic.java
@@ -0,0 +1,15 @@
+
+public class InterfaceStatic 
+{
+	public static void main(String ab[])
+	{
+		Zi.show();
+	}
+}
+interface Zi
+{
+	static void show()
+	{
+		System.out.println("Static Interface");
+	}
+}
