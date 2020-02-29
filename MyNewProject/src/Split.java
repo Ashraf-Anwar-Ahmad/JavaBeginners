76c8d8101f05728cbcead5aad4cb28866d333b2a
Anwar <ashrafanwar864@gmail.com>
2020-02-29 14:42:15 +0530

    String Split

---
+++ MyNewProject/src/Split.java
@@ -0,0 +1,11 @@
+
+public class Split 
+{
+	public static void main(String ab[])
+	{
+		String s="Anwar Aman Anmol Armaan";
+		String Names[]=s.split(" ");
+		for(String val : Names)
+		System.out.println(val);
+	}
+}
